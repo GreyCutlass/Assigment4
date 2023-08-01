74a63d690e7245c0d76ad11627e0bab4543a001c
GreyCutlass <Jashuasauri@gmail.com>
2023-07-31 21:38:13 -0400

    FirstCommit

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -4,11 +4,91 @@
 
 int extraMemoryAllocated;
 
-// implement merge sort
-// extraMemoryAllocated counts bytes of extra memory allocated
+void swap(int* firstVar, int* secondVar)
+{
+
+	int temp = *firstVar;
+	*firstVar = *secondVar;
+	*secondVar = temp;
+
+}
+
+void merge(int inputArray[], int left, int midPoint, int right)
+{
+	int mergeIndex = left, leftIndex = 0, rightIndex = 0;
+
+	int leftSize = midPoint - left + 1;
+	int rightSize = right - midPoint;
+
+	int *leftArray = (int*) malloc(sizeof(int) * leftSize);
+	extraMemoryAllocated = extraMemoryAllocated + (sizeof(int) * leftSize);
+	int *rightArray = (int*) malloc(sizeof(int) * rightSize);
+	extraMemoryAllocated = extraMemoryAllocated + (sizeof(int) * rightSize);
+
+	for(int index = 0; index < leftSize; ++index)
+		leftArray[index] = inputArray[left + index];
+	for(int index = 0; index < rightSize; ++index)
+		rightArray[index] = inputArray[midPoint + 1 + index];
+
+	while(leftIndex < leftSize && rightIndex < rightSize)
+	{
+
+		if(leftArray[leftIndex] <= rightArray[rightIndex])
+		{
+
+			inputArray[mergeIndex] = leftArray[leftIndex];
+			++leftIndex;
+
+		}
+		else
+		{
+
+			inputArray[mergeIndex] = rightArray[rightIndex];
+			++rightIndex;
+
+		}
+
+		++mergeIndex;
+
+	}
+
+	while(leftIndex < leftSize)
+	{
+
+		inputArray[mergeIndex] = leftArray[leftIndex];
+		++mergeIndex;
+		++leftIndex;
+
+	}
+	while(leftIndex < leftSize)
+	{
+
+		inputArray[mergeIndex] = leftArray[leftIndex];
+		++mergeIndex;
+		++leftIndex;
+
+	}
+
+	free(leftArray);
+	free(rightArray);
+
+
+}
+
 void mergeSort(int pData[], int l, int r)
 {
 	
+	if(l < r)
+	{
+
+		int midPoint = (l + r)/2;
+		mergeSort(pData, l, midPoint);
+		mergeSort(pData, midPoint+1, r);
+		merge(pData, l, midPoint, r);
+
+
+	}
+
 }
 
 // implement insertion sort
@@ -16,20 +96,79 @@ void mergeSort(int pData[], int l, int r)
 void insertionSort(int* pData, int n)
 {
 	
+	for(int index = 0; index < n; ++index)
+	{
+		int inIndex = index;
+		while(inIndex > 0  && pData[inIndex-1] > pData[inIndex])
+		{
+
+			swap(&pData[inIndex], &pData[inIndex - 1]);
+
+			inIndex = inIndex - 1;
+		}
+
+	}
+
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+	unsigned short swapOccured = 0;
+	for(int outerIndex = n-1; outerIndex >= 0; --outerIndex)
+	{
+
+		for(int innerIndex = 0; innerIndex < outerIndex; ++innerIndex)
+		{
+
+			if(pData[innerIndex] > pData[innerIndex+1])
+			{
+
+				swap(&pData[innerIndex], &pData[innerIndex+1]);
+				swapOccured = 1;
+
+			}
+
+		}
+		//Takes too long without this
+		if(!swapOccured)
+		{
+
+			break;
+
+		}
+
+	}
+
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	int minNumberIndex;
+
+	for(int minIndex = 0; minIndex < n; ++minIndex)
+	{	
+		minNumberIndex = minIndex;
+		for(int rolIndex = minIndex; rolIndex < n; ++rolIndex)
+		{
+
+			if(pData[rolIndex] < pData[minNumberIndex])
+			{
+
+				minNumberIndex = rolIndex;
+
+			}
+
+		}
+
+		swap(&pData[minIndex], &pData[minNumberIndex]);
+
+
+	}
+
 }
 
 // parses input file to an integer array
@@ -43,7 +182,7 @@ int parseData(char *inputFileName, int **ppData)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)malloc(sizeof(int) * dataSz);
-		// Implement parse data block
+		for(int index = 0; index < dataSz; ++index) fscanf(inFile, "%d ", *ppData+index);
 	}
 	
 	return dataSz;
